231f436d61c4c0734ba4c403781e10ef1fe6f609
Manojit Nandi <mnandi92@gmail.com>
2013-08-19 10:31:27 -0500

    Re-formatted the folder

--- setup.py
+++ setup.py
@@ -10,9 +10,17 @@ def download_lda_package():
 
 
 setup(
-    name="PythonSLDA",
-    packages=['PythonSLDA'],
+    name='PythonSLDA',
+    version='0.1.0',
+    author='Manojit Nandi',
+    author_email='mnandi92@gmail.com',
+    packages=['pythonslda', 'pythonslda.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/PythonSLDA/',
+    license='LICENSE.txt',
+    description='Python wrapper for sLDA method in R \"lda\" package.',
+    long_description=open('README.txt').read(),
     install_requires=[
-        'pyper',
-    ]
+        "pyper",
+    ],
 )
